98bdfc34e766830d4c0417a3966c78c4ca401f43
irackson <nasirus@protonmail.com>
2021-07-19 22:13:20 -0400

    dart resistor-color-duo pass

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,46 @@
 class ResistorColorDuo {
-  // Put your code here
+  List<String> colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
+  int value(List<String> arr) {
+    return int.parse("${colors.indexOf(arr[0])}${colors.indexOf(arr[1])}");
+  }
 }
+
+//* nicer solution from https://exercism.io/tracks/dart/exercises/resistor-color-duo/solutions/0608f3f6d1584e85ba5aca635d310420
+/* class ResistorColorDuo {
+  int value(List<String> input) {
+    try {
+      if (colorCodes[input[0]] != null && colorCodes[input[1]] != null) {
+        return int.parse('${colorCodes[input[0]]!}${colorCodes[input[1]]!}');
+      } else {
+        throw Exception;
+      }
+    } on Exception catch (_){
+      rethrow;
+    }
+  }
+
+  static Map<String, int> colorCodes = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+  };
+
+} */
