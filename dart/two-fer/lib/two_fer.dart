35aaa4e6d9aec4ba7efe8fa044fb1f4e4aa12d23
irackson <nasirus@protonmail.com>
2021-07-19 21:49:35 -0400

    dart two_fer pass

--- dart/two-fer/lib/two_fer.dart
+++ dart/two-fer/lib/two_fer.dart
@@ -1,3 +1,6 @@
-String twoFer() {
-  // Put your code here
+String twoFer([String str]) {
+  if (str is String && str.isNotEmpty) {
+    return "One for ${str}, one for me.";
+  }
+  return "One for you, one for me.";
 }
