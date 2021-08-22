c9ad3492eb868f2fca1a64bbcad0cce542d173d1
irackson <nasirus@protonmail.com>
2021-08-21 21:16:34 -0400

    linked-list ts

---
+++ typescript/linked-list/linked-list.ts
@@ -0,0 +1,36 @@
+// export class LinkedList<T> {
+//     linkedlist: T = [];
+//     constructor() {
+//         // if ('5'<T>') {
+//         // console.log('hi');
+//         // }
+//         // console.log(LinkedList.);
+//         // console.log(LinkedList);
+//         this.linkedlist: [];
+//     }
+
+//     pop(): number {
+//         return 20;
+//     }
+
+//     shift(): number {
+//         return 20;
+//     }
+
+//     unshift(num: number): number {
+//         return num;
+//     }
+
+//     push(num: number): number {
+//         return num;
+//     }
+
+//     count(): number {
+//         return 20;
+//     }
+
+//     delete(num: number): number {
+//         return num;
+//     }
+// }
+// const list = new LinkedList<number>();
